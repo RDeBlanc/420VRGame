080848a0400f05e5400fd16e3e37365cffd62502
Cameron <cst1493@gmail.com>
2019-10-09 00:17:42 -0500

    Progress?  Made it teleport.
    it's moving, I just can't get Vector3.Lerp to work the way I want it to.

--- Assets/Scripts/weapons/LS_Cylinder.cs
+++ Assets/Scripts/weapons/LS_Cylinder.cs
@@ -19,26 +19,24 @@ public class LS_Cylinder : MonoBehaviour
 
     void Start()
     {
+        //Debug.Log("Press Space to extend LightSaber. (LS_Cylinder.cs)");
         if (beam_Width == 0) beam_Width = 0.15f; //default settings
         if (beam_Length == 0) beam_Length = 2f;
         if (extendSpeed == 0) extendSpeed = 5;
 
     }
-
-
     void Update()
     {
         //temp = transform.localScale;
         //temp.y = 2f;
 
-
         if (Input.GetKeyDown(KeyCode.Space)) //extends/de-tends light on button press.          //TODO change later to work with VR controls instead of spacebar.
         {
             if (isActivated == true)
             {
-                isActivated = false;
+                isActivated = false; //TODO optional play sound here.
             }
-            else isActivated = true;
+            else isActivated = true; //TODO optional play sound here.
         }
         if (isActivated == true)
         {
@@ -47,7 +45,6 @@ public class LS_Cylinder : MonoBehaviour
             //endPosition.localPosition = Vector3.Lerp(endPosition.localPosition, extendedPosition, Time.deltaTime * (extendSpeed));
         }
         else transform.localScale = new Vector3(x: beam_Width, y: 0.01f, z: beam_Width);
-
 
 
         //line.SetPosition(0, startPosition.position); //these are the 2 inputs for start-position and end-positon in Unity.  This updates the light position.
